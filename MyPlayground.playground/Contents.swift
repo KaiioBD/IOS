ec942b243ff08001d16a345bd860c5da2a53fc8c
Facultad de Contaduría y Administración <facultaddecontaduriayadministracion@MacBook-15.local>
2025-08-18 13:00:53 -0600

    18 agosto 2025

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+
+let score: Float = 1.0
+let score2: Double = 1.0
+
+let max: Double = 1000000000000000000000000000000000000000000000000000000000000000000000000
+
+
+print(type(of: score))
+print(type(of: score2))
+print(type(of: max))
+
+
